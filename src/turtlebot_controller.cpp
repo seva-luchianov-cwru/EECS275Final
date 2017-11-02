efa70cddd9166b4498163339819545b8f33591ff
Seva Luchianov <vvl2@case.edu>
2017-11-02 16:23:15 -0400

    Updated controller to state from lab 7
    Signed-off-by: Seva Luchianov <vvl2@case.edu>

--- src/turtlebot_controller.cpp
+++ src/turtlebot_controller.cpp
@@ -1,29 +1,48 @@
 #include "minimal_turtlebot/turtlebot_controller.h"
 
+bool backingUp = false;
+bool turning = false;
+bool ignoreBumper = false;
 
-void turtlebot_controller(turtlebotInputs turtlebot_inputs, uint8_t *soundValue, float *vel, float *ang_vel)
- {
-	//Place your code here! you can access the left / right wheel 
-	//dropped variables declared above, as well as information about
-	//bumper status. 
-	
-	//outputs have been set to some default values. Feel free 
-	//to change these constants to see how they impact the robot. 
+int actionCounter = 0;
 
-	*vel = 0.0; // Robot forward velocity in m/s
-	//0.7 is max and is a lot
-	*ang_vel = 0.2;  // Robot angular velocity in rad/s
-	//0.7 is max and is a lot 
-  
-	*soundValue = 0;
-  
-	//here are the various sound value enumeration options
-	//soundValue.OFF
-	//soundValue.RECHARGE
-	//soundValue.BUTTON
-	//soundValue.ERROR
-	//soundValue.CLEANINGSTART
-	//soundValue.CLEANINGEND 
+void turtlebot_controller(turtlebotInputs turtlebot_inputs, uint8_t *soundValue, float *vel, float *ang_vel) {
+    *vel = 0.1; // Robot forward velocity in m/s
+    *ang_vel = 0.0;  // Robot angular velocity in rad/s
+    *soundValue = 0;
+
+
+    // Sensor Condition
+    if (ignoreBumper == false && (turtlebotInputs.leftBumperPressed == 1 ||  turtlebotInputs.rightBumperPressed == 1 || turtlebotInputs.centerBumperPressed == 1)) {
+        backingUp = true;
+        ignoreBumper = true;
+    } 
+    
+    // Backing Up Condition
+    if (backingUp) {
+	*vel = -0.1;
+	*ang_vel = 0;
+        actionCounter++;
+        
+        if(actionCounter >= 30) {
+            backingUp = false;
+            actionCounter = 0;
+            turning = true;
+        }
+    }
+    
+    // Turning Condition
+    if (turning) {
+	*ang_vel = 0.1;
+        *vel = 0;
+        actionCounter++;
+        
+        if (actionCounter >= 30) {
+            turning = false;
+            ignoreBumper = false;
+            actionCounter = 0;
+        }
+    }
 
 }
 
